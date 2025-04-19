7168c2cedbd888f7a553c279b648a6637a20a82a
Alan <alan.melgar.f@gmail.com>
2025-04-19 09:51:42 -0400

    base de datoss

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ from datetime import timedelta
 
 class Config:
     # Usa la variable de entorno DATABASE_URL que Render genera automáticamente
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     # SQLALCHEMY_DATABASE_URI = "postgresql://si2_user:G8OQJC0Yt9WREGNUVylTxdnNv63RpO47@dpg-cvu0lr7gi27c73acfn20-a.oregon-postgres.render.com/si2"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:alan123@localhost:5432/pruebas"
+    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:alan123@localhost:5432/pruebas"
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
