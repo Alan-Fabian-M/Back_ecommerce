d7ed99cbab08ecc3f12a7a7e65ad9d55fa0f4234
Alan <alan.melgar.f@gmail.com>
2025-04-19 10:06:08 -0400

    areglitos

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,13 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    
     CORS(app)
+    
     app.config.from_object(Config)
     
-    CORS(app, resources={r"/api/*": {"origins": "http://localhost:4200"}})
+    # CORS(app, resources={r"/api/*": {"origins": "http://localhost:4200"}})
+    
     db.init_app(app)
     jwt = JWTManager(app)
     migrate = Migrate(app, db)
